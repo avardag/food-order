4d4ae0b8d2eb5506669a12fd5349a0f5e32b839a
avardag <steklofan@gmail.com>
2024-04-21 13:21:03 +0200

    client/ add Header comp

---
+++ client/src/components/Header/index.tsx
@@ -0,0 +1,40 @@
+import styles from './header.module.css';
+import {Link} from "react-router-dom";
+
+export default function Header() {
+    const user={
+        name: "John Doe",
+    }
+    const cart={
+        totalCount: 10
+    }
+    const logout=()=>{}
+    return (
+        <header className={styles.header}>
+            <div className={styles.container}>
+                <Link to="/" className={styles.logo}>Food Order!</Link>
+                <nav >
+                    <ul>
+                        {user? (
+                            <li className={styles.menu_container}>
+                                <Link to="/profile" >{user.name}</Link>
+                                <div className={styles.menu}>
+                                    <Link to="profile" >Profile</Link>
+                                    <Link to="orders" >Orders</Link>
+                                    <a onClick={logout}>Logout</a>
+                                </div>
+                            </li>
+                        ): (
+                            <Link to="login" >Login</Link>
+                        )
+                        }
+                        <li>
+                            <Link to="cart" >Cart {cart.totalCount>0 && <span className={styles.cart_count}>{cart.totalCount}</span>}</Link>
+
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
